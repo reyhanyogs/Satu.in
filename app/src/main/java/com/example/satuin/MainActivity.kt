98bb304a91cf2bd812f9965c2637e25ed3b9f8d4
Reyhan Yogastama <reyhan2411@gmail.com>
2021-03-20 16:31:19 +0700

    Add comment

--- app/src/main/java/com/example/satuin/MainActivity.kt
+++ app/src/main/java/com/example/satuin/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.satuin
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+// Test
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
