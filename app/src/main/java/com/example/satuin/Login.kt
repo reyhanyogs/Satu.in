9b1048059b84c23823903480915cee2008561c1f
Reyhan Yogastama <reyhan2411@gmail.com>
2021-03-26 20:24:54 +0700

    Modify login activity

--- app/src/main/java/com/example/satuin/Login.kt
+++ app/src/main/java/com/example/satuin/Login.kt
@@ -1,11 +1,18 @@
 package com.example.satuin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        val BalikDaftar = findViewById<Button>(R.id.DaftarButton)
+        BalikDaftar.setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+        }
     }
 }
