9639d603acceff17629f578c6428b53b3b7d48b3
Clint02 <kennethwaworuntu@gmail.com>
2021-03-26 13:07:29 +0800

    GTS2

---
+++ app/src/main/java/com/example/satuin/GettingStarted2.kt
@@ -0,0 +1,11 @@
+package com.example.satuin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GettingStarted2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_getting_started2)
+    }
+}
