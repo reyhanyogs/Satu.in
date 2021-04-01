e6903e046580b98c69d604d97e80dfae6c71cc9c
Reyhan Yogastama <reyhan2411@gmail.com>
2021-04-01 17:10:48 +0700

    Add profil usaha activity page

---
+++ app/src/main/java/com/example/satuin/ProfilUsaha.kt
@@ -0,0 +1,11 @@
+package com.example.satuin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ProfilUsaha : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_profil_usaha)
+    }
+}
