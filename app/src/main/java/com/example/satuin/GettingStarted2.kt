1345a2e7c907289f80cfed2e92cc6ca3a1379989
Reyhan Yogastama <reyhan2411@gmail.com>
2021-04-01 17:09:16 +0700

    Add more layout for various density

--- app/src/main/java/com/example/satuin/GettingStarted2.kt
+++ app/src/main/java/com/example/satuin/GettingStarted2.kt
@@ -1,11 +1,18 @@
 package com.example.satuin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class GettingStarted2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_getting_started2)
+        val Penjual = findViewById<Button>(R.id.penjualButton)
+        Penjual.setOnClickListener {
+            val i = Intent(this, Login::class.java)
+            startActivity(i)
+        }
     }
 }
--- app/src/main/java/com/example/satuin/Login.kt
+++ app/src/main/java/com/example/satuin/Login.kt
@@ -9,7 +9,7 @@ class Login : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        val BalikDaftar = findViewById<Button>(R.id.DaftarButton)
+        val BalikDaftar = findViewById<Button>(R.id.DaftarbuttonLogin)
         BalikDaftar.setOnClickListener {
             val intent = Intent(this, Register::class.java)
             startActivity(intent)
