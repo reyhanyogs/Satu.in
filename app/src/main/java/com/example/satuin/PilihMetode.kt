d960bb73e29c3a5b84e3bba2d767ad26111a059b
Clint02 <kennethwaworuntu@gmail.com>
2021-04-03 22:03:59 +0800

    PM to DMS and DMP

--- app/src/main/java/com/example/satuin/PilihMetode.kt
+++ app/src/main/java/com/example/satuin/PilihMetode.kt
@@ -1,12 +1,24 @@
 package com.example.satuin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import com.example.satuin.R
 
 class PilihMetode : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pilih_metode)
+        val keDMS = findViewById<Button>(R.id.btnsendiri)
+        keDMS.setOnClickListener {
+            val intent = Intent(this, DetailMakananSendiri::class.java)
+            startActivity(intent)
+        }
+        val keDMP = findViewById<Button>(R.id.btnpatungan)
+        keDMP.setOnClickListener {
+            val intent = Intent(this, DetailMakananPatungan::class.java)
+            startActivity(intent)
+        }
     }
 }
