46bd97c1e517d38c745972f3ef01b69625490016
Clint02 <kennethwaworuntu@gmail.com>
2021-04-03 20:51:34 +0800

    DMS to PM

--- app/src/main/java/com/example/satuin/DetailMakananSendiri.kt
+++ app/src/main/java/com/example/satuin/DetailMakananSendiri.kt
@@ -1,11 +1,18 @@
 package com.example.satuin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class DetailMakananSendiri : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail_makanan_sendiri)
+        val BalikPM = findViewById<Button>(R.id.btnbackdms)
+        BalikPM.setOnClickListener {
+            val intent = Intent(this, PilihMetode::class.java)
+            startActivity(intent)
+        }
     }
 }
