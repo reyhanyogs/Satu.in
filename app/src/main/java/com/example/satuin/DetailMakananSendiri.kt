1ef6f0506657f0bf263846c416be78148983de81
Clint02 <kennethwaworuntu@gmail.com>
2021-04-03 20:42:24 +0800

    Add UI Detail Makanan Patungan dan Sendiri (1/4)

---
+++ app/src/main/java/com/example/satuin/DetailMakananSendiri.kt
@@ -0,0 +1,11 @@
+package com.example.satuin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class DetailMakananSendiri : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_makanan_sendiri)
+    }
+}
