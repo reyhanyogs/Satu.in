bd02984a823b5398dd4ef2f88e12d0401e8890fc
Reyhan Yogastama <reyhan2411@gmail.com>
2021-04-10 00:38:25 +0700

    Add import

--- app/src/main/java/com/example/satuin/GettingStarted2.kt
+++ app/src/main/java/com/example/satuin/GettingStarted2.kt
@@ -1,9 +1,12 @@
 package com.example.satuin
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class GettingStarted2 : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_getting_started2)
